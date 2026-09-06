5c9d8fd7c027692113bf17a352761b6359e858e5
bobzap66 <nickgpfs@gmail.com>
2026-09-06 18:16:37 -0400

    Register folder gallery transformer

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,5 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { FolderGallery } from "./quartz/plugins/transformers/folderGallery"
 import { Explorer } from "@quartz-community/explorer"
 
 const dossierOrder = [
@@ -57,5 +58,7 @@ Explorer({
 })
 
 const config = await loadQuartzConfig()
+config.plugins.transformers.push(FolderGallery())
+
 export default config
 export const layout = await loadQuartzLayout()
