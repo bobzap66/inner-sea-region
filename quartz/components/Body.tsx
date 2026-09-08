af29982cec0c53207167aced67b05749e9f7959a
bobzap66 <nickgpfs@gmail.com>
2026-09-07 22:08:40 -0400

    Lock campaign body before spoiler opt-in

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,7 +1,27 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
-const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return <div id="quartz-body">{children}</div>
+function isCampaignPage(slug: string | undefined) {
+  if (!slug) return false
+  return /^campaigns\/(?:archived\/)?[^/]+(?:\/|$)/i.test(slug)
 }
+
+const Body: QuartzComponent = ({ children, fileData }: QuartzComponentProps) => {
+  const lockedByDefault = isCampaignPage(fileData.slug)
+  return (
+    <div id="quartz-body" class={lockedByDefault ? "campaign-spoiler-pending" : undefined}>
+      {children}
+    </div>
+  )
+}
+
+Body.css = `
+#quartz-body.campaign-spoiler-pending > * {
+  visibility: hidden !important;
+}
+
+#quartz-body.campaign-spoiler-pending .campaign-spoiler-gate {
+  visibility: visible !important;
+}
+`
 
 export default (() => Body) satisfies QuartzComponentConstructor
