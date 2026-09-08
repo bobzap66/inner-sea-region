786a4a61b288a8e33dd3646f7901e99ede810567
bobzap66 <nickgpfs@gmail.com>
2026-09-07 22:36:42 -0400

    Move spoiler gate to Quartz body root

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,27 +1,21 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { CampaignSpoilerGate } from "./CampaignSpoilerGate"
 
 function isCampaignPage(slug: string | undefined) {
   if (!slug) return false
   return /^campaigns\/(?:archived\/)?[^/]+(?:\/|$)/i.test(slug)
 }
 
-const Body: QuartzComponent = ({ children, fileData }: QuartzComponentProps) => {
+const Body: QuartzComponent = (props: QuartzComponentProps) => {
+  const { children, fileData } = props
   const lockedByDefault = isCampaignPage(fileData.slug)
+
   return (
     <div id="quartz-body" class={lockedByDefault ? "campaign-spoiler-pending" : undefined}>
+      {lockedByDefault && <CampaignSpoilerGate {...props} />}
       {children}
     </div>
   )
 }
-
-Body.css = `
-#quartz-body.campaign-spoiler-pending > * {
-  visibility: hidden !important;
-}
-
-#quartz-body.campaign-spoiler-pending .campaign-spoiler-gate {
-  visibility: visible !important;
-}
-`
 
 export default (() => Body) satisfies QuartzComponentConstructor
